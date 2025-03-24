b182c344728f24096aa355c948cbce0f13b91457
Insignificance <happywin855@gmail.com>
2025-03-24 21:57:11 +0500

    feat(adsserivce): add entity for ad

---
+++ adsservice/internal/core/entity/ad.go
@@ -0,0 +1,13 @@
+package entity
+
+import "github.com/shopspring/decimal"
+
+type Ad struct {
+	ID          int64
+	Name        string
+	Description string
+	CostPerDay  decimal.Decimal
+	Deposit     decimal.Decimal
+	PhotoPath   string
+	LandlordId  int64
+}

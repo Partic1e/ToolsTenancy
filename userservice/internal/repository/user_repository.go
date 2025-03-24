69c95be2e28a6362e90728068f4b26bb49c76321
Insignificance <happywin855@gmail.com>
2025-03-24 19:15:41 +0500

    feat(userservice): add user repository

---
+++ userservice/internal/repository/user_repository.go
@@ -0,0 +1,51 @@
+package repository
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/shopspring/decimal"
+	"userservice/internal/core/entity"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) GetOrCreateUser(tgID int64) (*entity.User, error) {
+	var user entity.User
+	var balanceStr string
+
+	err := r.db.QueryRow("SELECT id, tg_id, balance, email FROM users WHERE tg_id = $1", tgID).
+		Scan(&user.ID, &user.TgID, &balanceStr, &user.Email)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		defaultBalance := decimal.NewFromFloat(0.00)
+		err = r.db.QueryRow(
+			"INSERT INTO users (tg_id, balance) VALUES ($1, $2) RETURNING id, tg_id, balance, email",
+			tgID, defaultBalance).
+			Scan(&user.ID, &user.TgID, &balanceStr, &user.Email)
+
+		if err != nil {
+			return nil, fmt.Errorf("[UserService][Postgres] ошибка при создании пользователя: %v", err)
+		}
+
+		log.Printf("[UserService][Postgres] Создан новый пользователь с tg_id: %d", tgID)
+	} else if err != nil {
+		return nil, fmt.Errorf("[UserService][Postgres] ошибка при поиске пользователя: %v", err)
+	}
+
+	user.Balance, err = decimal.NewFromString(balanceStr)
+	if err != nil {
+		return nil, fmt.Errorf("[UserService][Postgres] ошибка преобразования баланса: %v", err)
+	}
+
+	return &user, nil
+}
